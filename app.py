d4ef6a84cb86874bfda9ed13bd89016bfbda4203
munkh-erdene1g4i <munkh-erdene1g4i@moes.edu.mn>
2026-09-25 23:44:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -280,8 +280,8 @@ def get_audio():
   return jsonify({"error": "Аудио файл олдсонгүй"}), 404
 
 
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000, debug=True)
-
 if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run(host='0.0.0.0', port=5000, ssl_context='adhoc')
